90acdd895396f8f38a7679a99c4226eb6fc9e3f0
Igormm <60266683+Igormm@users.noreply.github.com>
2020-01-29 23:03:40 +0300

    Update tsk1.py

--- tsk1.py
+++ tsk1.py
@@ -7,5 +7,6 @@ str2 = os.path.basename(__file__)
 if str1 and len(str1) < 1000 and str1[0].isalpha():
  print(str1.title())
 else:
+ print(str1)
  print (f'Usage: {str2} "name lastname"\nThe first argument must be wrapped with quotation marks " "\nString cannot be empty and don\'t containing over 1000 characters')
  
